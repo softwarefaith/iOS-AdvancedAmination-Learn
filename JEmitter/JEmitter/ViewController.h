b5122ec142708da77f76808a1abbaf058fb41637
Alan <softwarefaith@126,con>
2016-07-20 17:13:47 +0800

    烟花效果

---
+++ JEmitter/JEmitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JEmitter
+//
+//  Created by 蔡杰Alan on 16/7/20.
+//  Copyright © 2016年 Allan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JEmitter/JEmitter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JEmitter
+//
+//  Created by 蔡杰Alan on 16/7/20.
+//  Copyright © 2016年 Allan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
